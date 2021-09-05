87603e14cd7ce90a3607e318250ffdb9dc61cd20
kmh <minghaokong77@163.com>
2021-09-06 00:57:29 +0800

    改为更新10天数据

--- src/main/java/cn/widealpha/train/service/ScheduleService.java
+++ src/main/java/cn/widealpha/train/service/ScheduleService.java
@@ -15,6 +15,13 @@ public class ScheduleService {
     @Scheduled(cron = "0 0 2 * * ?")
     public void copyStationWayBase() {
         LocalDate date = LocalDate.now();
-        System.out.println("更新数据成功,共更新" + stationWayMapper.copyStationBase(date.toString()) + "条数据");
+        for (int i = 0; i < 10; i++) {
+            try {
+                LocalDate d1 = date.plusDays(1);
+                System.out.println("更新" + d1 + "数据成功,共更新" + stationWayMapper.copyStationBase(d1.toString()) + "条数据");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
